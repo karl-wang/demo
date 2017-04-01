c5b1c6ba3a1ffb48b7c2b07ed00216fefed42aea
karl <360027026@qq.com>
2017-04-01 12:00:44 +0800

    init

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : MonoBehaviour
+{
+    public GameObject alienPrefab;
+
+    NetworkClient myClient;
+
+    // Create a client and connect to the server port
+    public void SetupClient()
+    {
+        ClientScene.RegisterPrefab(alienPrefab);
+
+        myClient = new NetworkClient();
+
+        myClient.RegisterHandler(MsgType.Connect, OnConnected);
+        myClient.Connect("127.0.0.1", 4444);
+    }
+
+    void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Client connected");
+    }
+}
